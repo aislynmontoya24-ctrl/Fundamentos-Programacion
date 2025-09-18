b0ffd7c2a32607c4ef5b25803665e6e6e84224a6
Mary Montoya <aislynmontoya24@gmail.com>
2025-09-18 07:56:17 -0600

    Lista 1

---
+++ Unidad 2/lista1.py
@@ -0,0 +1,20 @@
+#Declarar variables
+lista=["Juan","Ivan","Adrian","Jair"]
+print (lista)
+#imprimir ista 
+print (lista [2])
+#agregar a la lista
+lista.append("Alexa")
+print (lista)
+
+lista.insert(1,"Javier")
+print (lista)
+#sustituir 
+lista.pop()
+print (lista)
+
+lista.remove("Juan")
+print (lista)
+
+lista.pop(1)
+print (lista)
